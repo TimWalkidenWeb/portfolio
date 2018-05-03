32065032257d26652854d260093148a895fc3eb3
Tim <22911201@go.edgehill.ac.uk>
2018-05-03 17:41:38 +0100

    Content added and styling completed with comments starting

--- Portfolio/Gulpfile.js
+++ Portfolio/Gulpfile.js
@@ -1,12 +1,20 @@
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+
+gulp.task('styleWork', function() {
+    gulp.src('sass/styleWork.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/style_work.css'));
+});
 
 gulp.task('styles', function() {
     gulp.src('sass/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/style2.css'));
 });
+
+
 
 gulp.task('default',function() {
     gulp.watch('sass/**/*.scss',['styles']);
